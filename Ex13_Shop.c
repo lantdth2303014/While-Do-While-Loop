b86f4a76b8100475b96346ee3e7b66f3cb84816b
lantdth2303014 <133187878+lantdth2303014@users.noreply.github.com>
2023-05-23 11:13:01 +0700

    Add files via upload

--- Ex13_Shop.c
+++ Ex13_Shop.c
@@ -3,6 +3,7 @@
 
 int main(){
 	
+	int myRunes = 96367;
 	int order;
 	int total = 0;
 	int count_radagon = 0;
@@ -48,14 +49,20 @@ int main(){
 		}
 	} while (order >= 1 && order <= 4);								// order <= 4 cuz ye need to press 5 to exit
 	
-	printf("-- Order Details --\n");
-	printf("Radagon's Soreseal: %d\n", count_radagon);
-	printf("Shabriri Grapes: %d\n",count_shabriri);
-	printf("Erdtree's Favor: %d\n",count_erdtree);
-	printf("Starscourge Heirloom: %d\n",count_starscourge);
-	printf("Total: %d Runes\n\n", total);
-	printf("M\'Lord! I hope these items will help thou be the next Elden Lord.");
-	printf("\n");
+	if(myRunes < total){
+		printf("M\'Lord... I could not deliver thy wish.\n");
+	}
+	else{
+		printf("-- Order Details --\n");
+		printf("Radagon's Soreseal: %d\n", count_radagon);
+		printf("Shabriri Grapes: %d\n",count_shabriri);
+		printf("Erdtree's Favor: %d\n",count_erdtree);
+		printf("Starscourge Heirloom: %d\n",count_starscourge);
+		printf("Total: %d Runes\n\n", total);
+		printf("M\'Lord! I hope these items will help thou be the next Elden Lord.");
+		printf("\n");
+	}
+	
 	
 	getch();
 	return 0;
